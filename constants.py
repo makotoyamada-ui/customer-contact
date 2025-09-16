1193dc09ca05ef2432a8a6fa8b0b27d35db14e86
makotoyamada-ui <228154574+makotoyamada-ui@users.noreply.github.com>
2025-09-16 10:00:18 +0900

    chore: log dir -> /tmp/logs to avoid /mount permission error

--- constants.py
+++ constants.py
@@ -51,7 +51,7 @@ FEEDBACK_THANKS_MESSAGE = "ご回答いただき誠にありがとうござい�
 # ==========================================
 # ログ出力系
 # ==========================================
-LOG_DIR_PATH = "/mount/data/logs"
+LOG_DIR_PATH = "/tmp/logs" 
 LOGGER_NAME = "ApplicationLog"
 LOG_FILE = "application.log"
 
